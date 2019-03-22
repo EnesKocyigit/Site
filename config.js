2fc238ddb77a0075a1e5de8f386b90aeba32c326
EnesKocyigit <48841361+EnesKocyigit@users.noreply.github.com>
2019-03-22 14:49:09 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+  "ownerID": "393048048269262860", //kendi IDınızı yazınız
+  "admins": ["393048048269262860"],
+  "support": ["393048048269262860"],
+  "token": "NTU4MDEyODg0ODkwODEyNDMw.D3RH9g.PBSff2KuUPtkAwPxY47u9coSE3I", //botunuzun tokenini yazınız
+  "dashboard" : {
+    "oauthSecret": "hDS6TkPvsmYbKQwqepKby-RVGXUd6zyE", //botunuzun secretini yazınız 
+    "sessionSecret": "xyzxyz", //kalsın
+    "domain": "https://frepbot-web.glitch.me" //site URLnizi yazınız!
+  }
+};
+
+module.exports = config;
